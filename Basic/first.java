ba2974005e167e7c6fe248b234d73b5182598d58
Abhijit-garedi <paljustwin@gmail.com>
2024-12-31 14:58:23 +0530

    first program

---
+++ Basic/first.java
@@ -0,0 +1,18 @@
+class first {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+
+        
+
+        System.out.println("*");
+
+        System.out.println("**");
+
+        System.out.println("***");
+
+        System.out.println("****");
+
+        System.out.println("*****");
+    }
+}
+
