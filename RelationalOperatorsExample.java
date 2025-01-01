3edb8bd2e56494df179afea2fb43ec02277ab7ce
Abhijit-garedi <paljustwin@gmail.com>
2025-01-01 22:08:05 +0530

    Relational operator class

---
+++ RelationalOperatorsExample.java
@@ -0,0 +1,13 @@
+public class RelationalOperatorsExample {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+        System.out.println("a > b: " + (a > b));   // true
+        System.out.println("a < b: " + (a < b));   // false
+        System.out.println("a >= b: " + (a >= b)); // true
+        System.out.println("a <= b: " + (a <= b)); // false
+    }
+}
+
