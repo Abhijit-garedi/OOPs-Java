d26d1c9f79ab4f504021fb8be0f644321fee4f1a
Abhijit-garedi <paljustwin@gmail.com>
2025-01-04 15:21:29 +0530

    variable example program

---
+++ Basic/FirstVariable.java
@@ -0,0 +1,30 @@
+ public class FirstVariable {
+    public static void main(String[] args) {
+
+        String name = "Abhijit";
+
+        int x = 5;
+
+        float y = 4.6f;
+
+        char ch = 'A';
+
+        char xyz = 'O';
+
+        boolean bl = true;
+
+        boolean fl = false;
+
+        System.out.println("name:" +name);
+        System.out.println("Name: " + name);
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("ch: " + ch);
+        System.out.println("xyz: " + xyz);
+        System.out.println("bl: " + bl);
+        System.out.println("fl: " + fl);
+
+            
+    }
+        
+}
