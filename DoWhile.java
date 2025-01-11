d2a1dc9304dd5a8682330e327c014a6f04dff227
Abhijit-garedi <paljustwin@gmail.com>
2025-01-11 20:57:24 +0530

    Do while loop

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+public class DoWhile {
+    public static void main(String[] args){
+        int i = 12;
+        while( i < 11){
+            System.out.println("Avijit");
+        }
+        do { 
+            System.out.println("Hello World");
+
+        } while (i < 11);
+    }
+}
