11da1a4057df816069090e164e07e9815bcde901
Abhijit-garedi <paljustwin@gmail.com>
2025-01-12 21:45:19 +0530

    Correction left

---
+++ ForLoop11.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class ForLoop11 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<11; i++){
+            //System.out.println(i*n);
+        }
+        System.out.println(i*n);
+    }
+}
+
+
+/* correction required */
+  // output 5
+    
+
+   
+   
