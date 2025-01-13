bf5eb44417be42836cc131c94cf5c82769539dfe
Abhijit-garedi <paljustwin@gmail.com>
2025-01-13 11:38:02 +0530

    General C program

---
+++ Curiosity.c
@@ -0,0 +1,21 @@
+ #include  <stdio.h>
+  int main ()
+  {
+    int a , b, c, d;
+
+    printf("eneter the first nubmer:");
+    scanf("%d", &a);    
+
+    printf("enter the second number:");
+    scanf("%d", &b);
+
+
+    c = a + b;
+    d = c + b;
+
+    printf("sum of two number is %d", c);
+
+    printf("\nsum of three number is %d", d);
+
+    
+  }
