7069772954aa1cae9f313fe54c518ceddfc8a2bc
Abhijit-garedi <paljustwin@gmail.com>
2025-01-14 18:44:12 +0530

    To Print Name 10 times

---
+++ PrintName.java
@@ -0,0 +1,21 @@
+public class PrintName {
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println("Abhijit");
+        }
+    }
+    
+}
+
+
+/*Output
+Abhijit
+Abhijit
+Abhijit
+Abhijit
+Abhijit
+Abhijit
+Abhijit
+Abhijit
+Abhijit
+ */
