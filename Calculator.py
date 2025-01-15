a8b04b8f0c64c191c52546ba32b35bc030f6ba53
Abhijit-garedi <paljustwin@gmail.com>
2025-01-15 10:33:27 +0530

    simple

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+n = int(input("enter a number to print in words:"))
+
+if n == 1:
+    print("One")
+
+elif n == 2:
+
+    print("Two")
+
+elif n == 3:
+    print("Three")
+
+elif n == 4:
+    print("Four")
+
+else:
+    print("Wo mai kal batunga:")  
+
+    # Banana ky chahta tha vann ky gaya sad song sunn ky aya tha. 
+                 
