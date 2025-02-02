e4ec08485f6f7fe4fdf86462af42a5b75cb4d90d
Abhijit-garedi <paljustwin@gmail.com>
2025-02-02 12:20:31 +0530

    expr java program little correction done

---
+++ demo/expr.java
@@ -0,0 +1,21 @@
+//package is optional
+package demo;
+
+package demo;
+
+class Exp {
+    static String name; // Static variable
+    int age;
+
+    void display() {
+        System.out.println();
+    }
+}
+
+public class expr {
+    public static void main(String args[]) {
+        Exp.name = "hello "; 
+        Exp e = new Exp(); 
+        e.age = 10; 
+}
+}
