a4df785c75a212315340465e30e25de600225979
Abhijit-garedi <paljustwin@gmail.com>
2025-03-01 22:26:21 +0530

    Mulit level inheritance

---
+++ Multi_level_inheritance.java
@@ -0,0 +1,43 @@
+
+ class A{  // Parent class
+    int a;
+    
+    A() {
+        a = 10;
+    }
+}
+
+// child class 
+class B extends A {  
+    int b;
+    
+    B() {
+        b = 20;
+    }
+}
+
+// grandchild class 
+class C extends B {  
+    int c;
+    
+    C() {
+        c = 30;
+    }
+
+    // method to display sum
+    void sum() {
+        System.out.println("Sum is: " + (a + b + c));
+    }
+}
+
+// Main Class
+public class Multi_level_inheritance  {  
+    public static void main(String args[]) {  
+        C obj = new C();  // creating object of C
+        obj.sum();  // calling sum() method
+    }  
+}
+
+
+// Output
+// sum is 60
