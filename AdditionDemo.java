fcebb67e20e2f82f1f16dfacc9a303e101dc738c
Abhijit-garedi <paljustwin@gmail.com>
2025-03-03 01:10:14 +0530

    simple class and object program in java

--- AdditionDemo.java
+++ AdditionDemo.java
@@ -1,6 +1,5 @@
-// Define a class
+
 class Cal {
-    // Data members (variables)
     int num1;
     int num2;
 
@@ -13,14 +12,14 @@ class Cal {
 // Main class
 public class AdditionDemo {
     public static void main(String[] args) {
-        // Creating an object of the Addition class
+        // creating an object of the Addition class
         Cal obj = new Cal();
 
         // Assigning values
         obj.num1 = 10;
         obj.num2 = 20;
 
-        // Calling method and displaying the result
+        // calling method and displaying the result
         System.out.println("Sum: " + obj.add());
     }
 }
