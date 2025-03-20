5258f8be2c109bac6b2ba506918b32fcc19faa9e
Abhijit-garedi <paljustwin@gmail.com>
2025-03-20 22:08:32 +0530

    Package Program sameclass.java

---
+++ P1/sameclass.java
@@ -0,0 +1,20 @@
+package P1;
+
+public class sameclass {
+    private int a = 10;
+    private int b = 20;
+
+    protected int c = 30;
+    public int d = 40;
+
+    public sameclass(){
+        System.out.println("a value is" +a);
+        System.out.println("b value is" +b);
+        System.out.println("c value is" +c);
+        System.out.println("d value is" +d);
+
+    }
+    
+}
+
+
