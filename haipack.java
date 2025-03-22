df9349aa62dcedab6eac9c085803dce0f7bea786
Abhijit-garedi <paljustwin@gmail.com>
2025-03-22 23:52:40 +0530

    haipack Main Class

---
+++ haipack.java
@@ -0,0 +1,15 @@
+public class haipack {
+    public static void main(String[] args) {
+        firstp obj1 = new firstp();
+        secondp obj2 = new secondp();
+        thirdp obj3 = new thirdp();
+        thridp obj4 = new fourthp();
+        fifthp obj5 = new fifthp();
+
+        obj1.add();
+        obj2.sub();
+        obj3.mul();
+        obj4.div();
+        obj5.mod();
+    }
+}
